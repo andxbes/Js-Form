2bc84201177446b3757255956d71529abb808002
Andrey <andxbes@gmail.com>
2015-09-22 16:09:37 +0300

    перевел на jquery

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,102 +1,104 @@
 window.console.log("include");
+var checkTheForm;
 
-var checkTheForm = new CheckTheForm();
+$(document).ready(function () {
+    checkTheForm = new CheckTheForm();
+
+});
+
 
 
 function CheckTheForm() {
+    //ссылка на текущий объект CheckTheForm
     var obj = this;
-    window.console.log("constructor");
-    var loginField = document.getElementById("login");
-    var passField = document.getElementById("pass");
-    var repassField = document.getElementById("r_pass");
-    var emailField = document.getElementById("email");
+    //задаем переменные для работы (поля формы)
+    var $loginField = $("#login");
+    var $passField = $("#pass");
+    var $repassField = $("#r_pass");
+    var $emailField = $("#email");
 
 
 
-
+    //вешаем обработчики событий 
     this.addActionCheckedField = function () {
 
         window.console.log("addActions");
-        loginField.oninput = function () {
+        
+        $loginField.on("input", function () {
             obj.checkLogin(getLastWord(this));
-        };
+        });
 
-        passField.oninput = function () {
+        $passField.on("input", function () {
             obj.checkpass(getLastWord(this), this);
-        };
+        });
 
-        repassField.oninput = function () {
+        $repassField.on("input", function () {
             obj.checkpass(getLastWord(this), this);
-        };
+        });
 
-        emailField.oninput = function () {
+        $emailField.on("input", function () {
             obj.checkEmail(getLastWord(this));
-        };
+        });
 
     };
 
+ 
 
-
-
+    //получаем последний символ из строки 
     function getLastWord(ob) {
         var word = ob.value[ob.value.length - 1];
         window.console.info(word);
         return word;
-
     }
-
+    //задаем параметры для проверки "живого" ввода данных 
     this.checkLogin = function (word) {
         var patt = new RegExp("[a-z0-9A-Z]");
-        actionEntry(loginField, patt.test(word));
+        actionEntry($loginField, patt.test(word));
     };
-
-
 
     this.checkpass = function (word, ob) {
         var patt = new RegExp("[a-z0-9A-Z]");
-        actionEntry(ob, patt.test(word));
+        actionEntry($(ob), patt.test(word));
     };
 
     this.checkEmail = function (word) {
         var patt = new RegExp("[a-z0-9\.@A-Z]");
-        actionEntry(emailField, patt.test(word));
+        actionEntry($emailField, patt.test(word));
     };
 
+     //действие при корректном или не корректном вводе 
+    function actionEntry($ob, isTrue) {
 
-    function actionEntry(ob, isTrue) {
+        var error = "error";
 
         if (isTrue) {
-            if (ob.hasAttribute("class"))
-                ob.removeAttribute("class");
+            $ob.removeClass(error);
         } else {
-            ob.setAttribute("class", "error");
-            ob.value = "";
+            $ob.addClass(error);
+            //убрал стираниее всей строки , я видимо не правильно понял тогда ДЗ
+            $ob.val($ob.val().slice(0, -1));
         }
         return isTrue;
     }
 
-
+    //проверка перед отправкой 
     this.submit = function () {
         window.console.log("submit");
         var regLogin = new RegExp("^[a-zA-z]{1}[a-zA-Z1-9]{3,20}$");
         var regmail = new RegExp("^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$");
         var regPass = new RegExp("[a-zA-Z1-9]{6,20}");
-        
-        // window.console.info(loginField.value + "   " + regLogin.test(loginField.value));
-            // window.console.info(passField.value + "   " + regPass.test(passField.value));
-                // window.console.info(emailField.value + "   " + regmail.test(emailField.value));
 
-        if (actionEntry(loginField, regLogin.test(loginField.value))
-                && actionEntry(emailField, regmail.test(emailField.value))
-                && actionEntry(passField, regPass.test(passField.value))
-                && actionEntry(repassField, regPass.test(repassField.value))
-                && actionEntry(passField, (repassField.value == passField.value))) {
+        if (actionEntry($loginField, regLogin.test($loginField.val()))
+                && actionEntry($emailField, regmail.test($emailField.val()))
+                && actionEntry($passField, regPass.test($passField.val()))
+                && actionEntry($repassField, regPass.test($repassField.val()))
+                && actionEntry($passField, ($repassField.val() === $passField.val()))) {
             send();
         }
     };
-
+    //отправка
     function send() {
-        window.console.info("send");
+        window.console.info(" Very  Well !!!");
     }
 
 
